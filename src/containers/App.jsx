72e5b677af963091aab1f212287457f5756aa14e
Solej77 <ing.josgalo@gmail.com>
2020-01-14 23:24:58 -0600

    Project portfolio

---
+++ src/containers/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Main from '../components/Main';
+import Sidebar from '../components/Sidebar';
+import Info from '../components/Info';
+import About from '../components/About';
+import Social from '../components/Social';
+import Education from '../components/Education';
+import Experience from '../components/Experience';
+import Certificates from '../components/Certificates';
+import Skills from '../components/Skills';
+
+const App = () => {
+  return(
+    <Main>
+      <Sidebar>
+        <About />
+        <Social />
+      </Sidebar>
+      <Info>
+        <Education />
+        <Experience />
+        <Certificates />
+        <Skills />
+      </Info>
+    </Main>
+  );  
+}
+
+export default App;
